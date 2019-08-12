ab600f3af2d80448584661d6431c7e88e28ae67c
Steve Ripberger <sripberger@batterii.com>
2019-08-12 15:05:26 -0400

    Replace sinon star import with default import

--- generators/app/templates/test.ts
+++ generators/app/templates/test.ts
@@ -1,6 +1,6 @@
-import * as sinon from 'sinon';
 import { <%= name %> } from '../../lib/<%= moduleName %>';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 describe('<%= name %>', function() {
 	// Write unit tests for the <%= name %> class here.
